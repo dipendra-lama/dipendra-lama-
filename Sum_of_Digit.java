051d0e42dfa0f40c840ed387126ee8f813fc400d
GLBITM-25110CN351 <dipendralama38555@gmail.com>
2026-06-13 12:42:25 +0530

    Fix digits code

---
+++ Sum_of_Digit.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+/* sum of digut of programs*/
+//input number 126----->(1+2+6 = 9)
+class Sum_of_Digit{
+    public static void main(String[] args){
+        int n, r, Sum = 0;
+    System.out.print("Enter any number");
+        Scanner ref = new Scanner(System.in);
+        n = ref.nextInt();
+        while(n>0){
+            r= n%10;
+            Sum= Sum +r;
+            n = n/10;
+        }
+        System.out.print("Sum of Digits" + Sum);
+
+    }
+}
